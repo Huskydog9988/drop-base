43c55189c6f5fb1ad23a0523037659109355d56c
Huskydog9988 <39809509+Huskydog9988@users.noreply.github.com>
2026-01-09 14:43:28 -0500

    add typechecking

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,2 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({});
+export default defineNuxtConfig({
+  compatibilityDate: "2026-01-09",
+
+  typescript: {
+    typeCheck: true,
+  },
+});
