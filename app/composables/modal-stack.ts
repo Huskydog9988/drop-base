acd11dc7ef671df1adc871e1eeb4ff429ad053cd
Huskydog9988 <39809509+Huskydog9988@users.noreply.github.com>
2026-01-09 12:47:16 -0500

    use nuxt 4 codemod

--- app/composables/modal-stack.ts
+++ app/composables/modal-stack.ts
@@ -1,7 +1,7 @@
 import type { Component } from "vue";
-import ConfirmationModal from "../components/ConfirmationModal.vue";
-import NotificationModal from "../components/NotificationModal.vue";
-import TextInputModal from "../components/TextInputModal.vue";
+import ConfirmationModal from "../../components/ConfirmationModal.vue";
+import NotificationModal from "../../components/NotificationModal.vue";
+import TextInputModal from "../../components/TextInputModal.vue";
 
 export type ModalCallbackType<T extends ModalType> = (
   event: ModalEvents[T],
