6eff4a805c2d43548017a5fa5882fde1f5dafa62
Huskydog9988 <39809509+Huskydog9988@users.noreply.github.com>
2026-01-10 22:20:27 -0500

    use ts and eslint from drop repo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,18 @@
 export default defineNuxtConfig({
   compatibilityDate: "2026-01-09",
 
+  modules: ["@nuxt/eslint"],
+
   typescript: {
     typeCheck: true,
+
+    tsConfig: {
+      compilerOptions: {
+        verbatimModuleSyntax: false,
+        strictNullChecks: true,
+        exactOptionalPropertyTypes: false,
+        allowImportingTsExtensions: false,
+      },
+    },
   },
 });
